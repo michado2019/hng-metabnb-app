103947a4690f11a565522252faa13ea11850ab1c
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-14 11:41:41 +0100

    feat: contents added to SectionTwo component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import SectionOne from './SectionOne'
+import SectionTwo from './SectionTwo'
 
 export default function Home() {
   return (
     <div className='homeWrapper'>
         <SectionOne />
+        <SectionTwo />
     </div>
   )
 }
--- src/components/pages/SectionTwo.jsx
+++ src/components/pages/SectionTwo.jsx
@@ -4,7 +4,18 @@ import './SectionTwo.css'
 export default function SectionTwo() {
   return (
     <div className='sectionTwo-wrappper'>
-      
+      <div className='sectionTwo-divs'>
+        <img src='images/Vector (1).png' alt='img' className='sectionTwo-img' />
+        <h2 className='sectionTwo-text'>MBToken</h2>
+      </div>
+      <div className='sectionTwo-divs'>
+        <img src='images/image 66.png' alt='img' className='sectionTwo-img' id='metaMask-img'/>
+        <h2 className='sectionTwo-text' id='metaMask-text'>METAMASK</h2>
+      </div>
+      <div className='sectionTwo-divs'>
+        <img src='images/openSea.png' alt='img' className='sectionTwo-img' id='openSea-img' />
+        <h2 className='sectionTwo-text'>OpenSea</h2>
+      </div>
     </div>
   )
 }
