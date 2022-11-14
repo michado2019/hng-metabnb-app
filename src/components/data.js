3a408f2d0c1f70ca61c3c4dd696d0349b230549d
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-14 12:40:36 +0100

    feat: data for home page and place to stay created

---
+++ src/components/data.js
@@ -0,0 +1,110 @@
+const data = [
+    {
+        id: 1,
+        mainImg: 'images/Frame 151.png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:2,
+        mainImg: 'images/Frame 151 (1).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:3,
+        mainImg: 'images/Frame 151 (2).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:4,
+        mainImg: 'images/Frame 151 (3).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:5,
+        mainImg: 'images/Frame 151 (4).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:6,
+        mainImg: 'images/Frame 151 (5).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:7,
+        mainImg: 'images/Frame 151 (6).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:8,
+        mainImg: 'images/Frame 151 (7).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:9,
+        mainImg: 'images/Frame 151 (8).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:10,
+        mainImg: 'images/Frame 151 (9).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:11,
+        mainImg: 'images/Frame 151 (10).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    },
+    {
+        id:12,
+        mainImg: 'images/Frame 151 (11).png',
+        text1: 'Desert king',
+        text2: '1MBT per night',
+        text3: '2345km away',
+        text4: 'available for 2weeks stay',
+        img2: 'Star 2.png'
+    }
+] 
