9bdc450a7d66583e0032c8e889c1dd2b21083d1a
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-14 05:37:18 +0100

    feat: sectionOne contents added

--- src/components/pages/SectionOne.jsx
+++ src/components/pages/SectionOne.jsx
@@ -1,17 +1,57 @@
-import React from 'react'
-import './SectionOne.css'
+import React from "react";
+import "./SectionOne.css";
 
 export default function SectionOne() {
   return (
-    <div className='sectionOne-wrapper'>
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        Hellorrrrrttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
+    <div className="sectionOne-wrapper">
+      <div className="sectionOne-flex">
+        <div>
+          <h2>
+            Rent a <span className="sectionOne-span"> Place </span>away from
+            <span className="sectionOne-span"> Home </span>in the
+            <span className="sectionOne-span"> Metaverse</span>
+          </h2>
+          <p>
+            We provide you access to luxury and affordable houses in the
+            metaverse, get a chance to turn your imagination to reality at your
+            comfort zone
+          </p>
+          <form className="sectionOne-form">
+            <input
+              type="text"
+              placeholder="Search for location"
+              className="sectionOne-form_input"
+            />
+            <button className="sectionOne-form_btn">Search</button>
+          </form>
+        </div>
+        <div className="sectionOne-img_div">
+          <div className="sectionOne-img_flex1">
+            <img
+              src="images/image 4.png"
+              alt="img"
+              className="sectionOne-img"
+            />
+            <img
+              src="images/image 6.png"
+              alt="img"
+              className="sectionOne-img"
+            />
+          </div>
+          <div className="sectionOne-img_flex2">
+            <img
+              src="images/image 3.png"
+              alt="img"
+              className="sectionOne-img"
+            />
+            <img
+              src="images/image 5.png"
+              alt="img"
+              className="sectionOne-img"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
