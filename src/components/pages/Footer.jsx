9b32d6716b200597249127bfb90c32759273954c
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-15 11:02:25 +0100

    style: css apply to the SectionFour.jsx

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -8,11 +8,11 @@ export default function Footer() {
           <div className='footerFlex-one'>
             <div className='footerflex-one_1'>
             <img src='images/Vector (6).png' alt='footerLogo' className='footerLogo' />
-            <h2>Metabnb</h2>
+            <h2 className='footerLogo-text'>Metabnb</h2>
             </div>
             <div className='footerflex-one_2'>
             <img src='images/Vector (7).png' alt='footerIcon' className='footerSocial-img' />
-            <img src='images/Vector (8).png' alt='footerIcon' className='footerSocial-img' />
+            <img src='images/Vector (8).png' alt='footerIcon' className='footerSocial-img' id='vector8'/>
             <img src='images/Vector (9).png' alt='footerIcon' className='footerSocial-img' />
             </div>
             <div className='footerCopyright'>
@@ -20,21 +20,21 @@ export default function Footer() {
             </div>
           </div>
           <div className='footerFlex-two'>
-            <h2>Community</h2>
+            <h2 className='footerSub-titles'>Community</h2>
             <p>NFT</p>
             <p>Tokens</p>
             <p>Landlords</p>
             <p>Discord</p>
           </div>
-          <div className='footerFlex-three'>
-            <h2>Places</h2>
+          <div className='footerFlex-two'>
+            <h2 className='footerSub-titles'>Places</h2>
             <p>Castles</p>
             <p>Farms</p>
             <p>Beach</p>
             <p>Learn more</p>
           </div>
-          <div className='footerFlex-four'>
-            <h2>About us</h2>
+          <div className='footerFlex-two'>
+            <h2 className='footerSub-titles'>About us</h2>
             <p>Road map</p>
             <p>Creators</p>
             <p>Career</p>
