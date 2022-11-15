b84ace3b1b222fed40b89fb02db6efe8c5b66b15
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-15 09:02:36 +0100

    feat: Footer.jsx component adde to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './components';
+import { Navbar, Footer } from './components';
 import { AppRoutes } from './components/routes';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <AppRoutes />
+      <Footer />
     </div>
   );
 }
