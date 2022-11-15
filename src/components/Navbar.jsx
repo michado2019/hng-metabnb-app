bc192c0dcd7f67b5604b301b93eabc0a3c44d5fb
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-15 15:49:37 +0100

    feat, style: modal is added and styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { ArrowForwardIos, CancelOutlined } from "@mui/icons-material";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "600px",
+  height: "326px",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: "10px",
+  padding: "15px 25px",
+};
 const CustomNavLink = ({ to, ...props }) => {
   let activeStyle = {
     textDecoration: "none",
     color: "red",
     transition: "all 0.5s",
   };
-
   return (
     <NavLink
       style={({ isActive }) =>
@@ -22,6 +37,15 @@ const CustomNavLink = ({ to, ...props }) => {
 };
 
 export default function Navbar() {
+  //Modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  //Handle Wallet close
+  const handleWalletCancel = () => {
+    setOpen(false);
+  };
   return (
     <div className="navbarWrapper">
       <img src="images/Vector.png" alt="logo" className="logo" />
@@ -31,14 +55,63 @@ export default function Navbar() {
         <span id="logoText3">bnb</span>
       </h2>
       <div className="navLinks">
-        <CustomNavLink to="/" className='navLinksTexts'>Home</CustomNavLink>
-        <CustomNavLink to="/stay" className='navLinksTexts'>Place to stay</CustomNavLink>
-        <CustomNavLink to="/nfts" className='navLinksTexts'>NFTs</CustomNavLink>
-        <CustomNavLink to="/community" className='navLinksTexts'>Community</CustomNavLink>
+        <CustomNavLink to="/" className="navLinksTexts">
+          Home
+        </CustomNavLink>
+        <CustomNavLink to="/stay" className="navLinksTexts">
+          Place to stay
+        </CustomNavLink>
+        <CustomNavLink to="/nfts" className="navLinksTexts">
+          NFTs
+        </CustomNavLink>
+        <CustomNavLink to="/community" className="navLinksTexts">
+          Community
+        </CustomNavLink>
       </div>
       <div className="connectWallet-div">
-        <button className="connectWallet-btn">Connect wallet</button>
+        <button className="connectWallet-btn" onClick={handleOpen}>
+          Connect wallet
+        </button>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <div className="walletTop-flex">
+              <h4 className="walletTitle">Connect Wallet</h4>
+              <CancelOutlined
+                className="walletCancel"
+                onClick={handleWalletCancel}
+              />
+            </div>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <p className="walletSub-title">Choose your preferred wallet:</p>
+            <div className="walletPreferred">
+              <img
+                src="images/image 66 (1).png"
+                alt="wallet"
+                className="walletPreferred-img"
+              />
+              <h5>Metamask</h5>
+              <ArrowForwardIos className="walletArrow" />
+            </div>
+            <div className="walletPreferred">
+              <img
+                src="images/image 69.png"
+                alt="wallet"
+                className="walletPreferred-img"
+              />
+              <h5>Connect Wallet</h5>
+              <ArrowForwardIos className="walletArrow" />
+            </div>
+          </Typography>
+        </Box>
+      </Modal>
     </div>
   );
 }
