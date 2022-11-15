d4f8c95e7ec5f2b0de2c3839889283f286730880
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-15 06:36:55 +0100

    feat: SectionFour.jsx added to the app

---
+++ src/components/pages/SectionFour.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SectionFour.css'
+
+export default function SectionFour() {
+  return (
+    <div className='sectionFour-wrapper'>
+        SectionFour
+    </div>
+  )
+}
