aeb4c4c0754ad9de14e4681954225c03f0566abe
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-15 09:58:15 +0100

    feat: first row of the grid layout content added

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -4,7 +4,22 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='footerWrapper'>
-        Footer
+        <div className='footerGrid-layout'>
+          <div className='footerFlex-one'>
+            <div className='footerflex-one_1'>
+            <img src='images/Vector (6).png' alt='footerLogo' className='footerLogo' />
+            <h2>Metabnb</h2>
+            </div>
+            <div className='footerflex-one_2'>
+            <img src='images/Vector (7).png' alt='footerIcon' className='footerSocial-img' />
+            <img src='images/Vector (8).png' alt='footerIcon' className='footerSocial-img' />
+            <img src='images/Vector (9).png' alt='footerIcon' className='footerSocial-img' />
+            </div>
+            <div className='footerCopyright'>
+                <p>© 2021 Metabnb</p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
